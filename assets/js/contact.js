c791ce406874c0c7c0e39c700f102aa2c7567247
Giacomo Gualtieri <gcmgualtieri@gmail.com>
2022-11-05 16:00:21 +0100

    fixed contact form and added required to the input

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,27 +1,26 @@
-// function per emailJS
-function sendMail() {
-  var parameters = {
-    name: document.querySelector("#name").value,
-    email: document.querySelector("#email").value,
-    message: document.querySelector("#message").value,
-  };
-
-  // serviceID e templateID
-  const serviceID = "service_f8rpbka";
-  const templateID = "template_0fqf1z8";
-
-  emailjs
-    .send(serviceID, templateID, parameters)
-    .then((res) => {
-      document.querySelector("#name").value = "";
-      document.querySelector("#email").value = "";
-      document.querySelector("#message").value = "";
-
-      console.log(res);
-
-      alert(
-        "\nYour message have been sent successfully. I'll reply as soon as possible!\n\nIl tuo messaggio è stato inviato correttamente. Risponderò il prima possibile!\n"
-      );
-    })
-    .catch((err) => console.log(err));
-}
+// function per emailJS presa dai Docs del servizio stesso
+window.onload = function () {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      this.contact_number.value =
+        (Math.random() * 100000) | 0;
+      emailjs
+        .sendForm("contact_service", "contact_form", this)
+        // alert di invio effettuato o errore
+        .then(
+          function () {
+            alert(
+              "\nYour message have been sent successfully. I'll reply as soon as possible!\n\nIl tuo messaggio è stato inviato correttamente. Risponderò il prima possibile!\n"
+            );
+          },
+          function (error) {
+            console.log(
+              "There was an error. Retry!",
+              error
+            );
+          }
+        );
+    });
+};
